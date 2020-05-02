faa99b9a5a72102c5349bd695d811c101d6b3588
Chris Johnson <chrisjohnson112@yahoo.com>
2020-05-02 13:16:02 -0500

    first upload

---
+++ build/precache-manifest.9bd4be42802a83ac9bbd67d5c11dd6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d52afbda16dd2d7eea1d8a6ee79fcc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90465541454efb3a4b4c",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e784a2f3fc181bad4600",
+    "url": "/static/css/main.553b3f79.chunk.css"
+  },
+  {
+    "revision": "90465541454efb3a4b4c",
+    "url": "/static/js/2.71cea352.chunk.js"
+  },
+  {
+    "revision": "e64558e2f95ad6e9b52b3cecd9aea933",
+    "url": "/static/js/2.71cea352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e784a2f3fc181bad4600",
+    "url": "/static/js/main.b2abd9ba.chunk.js"
+  },
+  {
+    "revision": "97000337c8377e2241ad",
+    "url": "/static/js/runtime-main.d993e1f2.js"
+  }
+]);
